cabef95c8d92b8c1926cc73b02900a107ac53d41
Rizwan <rizwankassamali1@gmail.com>
2022-01-31 22:23:15 -0500

    done frontend and back end gets data

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,39 @@
 from flask import Flask
+from flask_mail import Mail,  Message
+from flask_cors import CORS
+from flask import request
 
-app = Flask(__name__)
+app = Flask(__name__, "/../frontend/build")
+
+app.config['MAIL_SERVER'] = 'smtp.mail.yahoo.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = 'rizwankassamali@yahoo.ca'
+app.config['MAIL_PASSWORD'] = '786National@3471'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+mail = Mail(app)
+CORS(app)
+
 
 @app.route('/submit/')
 def submit():
 
-	# This would be a good place to do something with the data you are
-	# submitting from the front-end!
+    # recv data from api
+    name = request.args.get('name')
+    email = request.args.get('email')
+    fsc = request.args.get('sc')
+    teamSize = request.args.get('team')
+    print(name)
+    print(email)
+    print(fsc)
+    print(teamSize)
+    
+    # send info to slack
 
-	return {'hello': 'world'}
-
+    # send email to the person
+    msg = Message('Hello from the other side!', sender='peter@mailtrap.io',
+                  recipients=['rizwankassamali1@gmail.com'])
+    msg.body = "Hey Paul, sending you this email from my Flask app, lmk if it works"
+    mail.send(msg)
+    
+    return {'hello': 'wofgdfgrld'}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,40 +1,77 @@
 import React from 'react';
-
 import './App.css';
 
-function App(this: any) {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends React.Component {
 
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name"></input><br></br>
-        
-        <label htmlFor="email">Email Address:</label>
-        <input type="email" id="email" name="email"></input><br></br>
 
-        <label htmlFor="lname">Favourite Source Control:</label>
-        
-          <select >
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      value: '',
+      playerName: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event: { target: { value: any; }; }) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event: any) {
+    console.log("making request")
+    fetch('/submit/')
+      .then(response => {
+        console.log(response)
+        return response.json()
+      })
+      .then(json => {
+        console.log = (json)
+        this.setState({ playerName: json[0] })
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit} action="http://localhost:5000/submit/" method="get">
+
+          <h1> Enter your information.</h1>
+
+          <label>Name: </label>
+          <input type="text" name="name" />
+          <br></br>
+          <br></br>
+
+          <label htmlFor="email">Email Address: </label>
+          <input type="email" id="email" name="email"></input>
+          <br></br>
+          <br></br>
+
+          <label htmlFor="sc">Favourite Source Control: </label>
+          <select name="sc" id="sc">
             <option value="github">GitHub</option>
             <option value="gitlab">GitLab</option>
             <option value="bitbucket">BitBucket</option>
             <option value="tfs">TFS</option>
             <option value="other">Other</option>
           </select>
-        <br></br>
+          <br></br>
+          <br></br>
 
-        <label htmlFor="lname">How many people work on your team?</label>
-        <input type="number" id="team" name="team"></input><br></br>
+          <label htmlFor="team">How many people work on your team? </label>
+          <input type="number" id="team" name="team"></input
+          ><br></br>
+          <br></br>
 
-        <input type="submit" value="Submit"></input>
+          <input type="submit" onChange={this.handleChange} value="Submit Info." />
 
-      </header>
-      <body>
-
-      </body>
-    </div>
-  );
+        </form>
+   
+      </div>
+    );
+  }
 }
 
 export default App;
